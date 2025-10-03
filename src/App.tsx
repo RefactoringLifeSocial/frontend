fcf48689f451b5a1d3ce4e06b6533097d70e4148
Nievas2 <angelgabrielnievas@gmail.com>
2025-10-03 11:29:35 -0300

    feat(Login): :sparkles: Add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
+import Login from "./pages/Login"
 
 function App() {
   return (
     <main>
       <Routes>
         <Route path="/" element={<h1>Social pet!</h1>} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </main>
   )
