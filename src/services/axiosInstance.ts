2799c0e43f9c60b88fdd9737e9f884a7e17fbaf9
Nievas2 <104918140+Nievas2@users.noreply.github.com>
2025-10-02 21:05:27 -0300

    feat: :tada: Init project (#1)

---
+++ src/services/axiosInstance.ts
@@ -0,0 +1,39 @@
+import axios, { AxiosError } from "axios"
+import Cookies from "js-cookie"
+
+const token = Cookies.get("token")
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+})
+
+axiosInstance.interceptors.request.use(
+  function (config: any) {
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+  },
+
+  function (error: any) {
+    return Promise.reject(error)
+  }
+)
+
+axiosInstance.interceptors.response.use(
+  function (response: any) {
+    return response
+  },
+  
+  function (error: AxiosError) {
+    /* if (error.response == undefined) {
+      window.location.href = "/mantenimiento"
+      return Promise.reject(error)
+    } */
+
+    return Promise.reject(error)
+  }
+)
+
+export default axiosInstance
