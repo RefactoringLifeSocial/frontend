5f8b2fcecefb6d9806b9b1f57303a081f1c17504
Nievas2 <angelgabrielnievas@gmail.com>
2025-10-08 14:24:38 -0300

    refactor(Auth): :bug: reorganize authentication routes and simplify AuthLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom"
 import Login from "./pages/auth/login/Login"
+import AuthLayout from "./layout/AuthLayout"
 
 function App() {
   return (
     <main>
       <Routes>
         <Route path="/" element={<h1>Social pet!</h1>} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+        </Route>
       </Routes>
     </main>
   )
--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,14 +1,10 @@
-import type { ReactNode } from "react"
+import { Outlet } from "react-router-dom"
 
-interface AuthLayoutProps {
-  children: ReactNode
-}
-
-export default function AuthLayout({ children }: AuthLayoutProps) {
+export default function AuthLayout() {
   return (
     <div className="min-h-screen bg-gray-50 flex items-center justify-center p-0 md:p-8">
       <div className="w-full min-h-screen md:min-h-0 md:max-w-md bg-white md:rounded-2xl md:shadow-xl">
-        {children}
+        <Outlet />
       </div>
     </div>
   )
