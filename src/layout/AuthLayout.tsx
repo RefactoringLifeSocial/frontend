b9567c8629acabdcdc4f441cd9b90dac5954e16d
Nievas2 <angelgabrielnievas@gmail.com>
2025-10-08 14:55:30 -0300

    feat(Auth): :sparkles: Implement header and footer

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,11 +1,19 @@
+import FooterAuth from "@/components/FooterAuth"
+import HeaderAuth from "@/components/HeaderAuth"
 import { Outlet } from "react-router-dom"
 
 export default function AuthLayout() {
   return (
-    <div className="min-h-screen bg-gray-50 flex items-center justify-center p-0 md:p-8">
-      <div className="w-full min-h-screen md:min-h-0 md:max-w-md bg-white md:rounded-2xl md:shadow-xl">
-        <Outlet />
+    <main className="flex flex-col bg-gray-50 min-h-screen">
+      <HeaderAuth />
+      
+      <div className="min-h-screen bg-gray-50 flex items-center justify-center p-0 md:p-8">
+        <div className="w-full min-h-screen md:min-h-0 md:max-w-md bg-white md:rounded-2xl md:shadow-xl">
+          <Outlet />
+        </div>
       </div>
-    </div>
+
+      <FooterAuth />
+    </main>
   )
 }
