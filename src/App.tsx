bcb595881b83b2f7e82ae28c082b1bdeaba0fbda
Nievas2 <angelgabrielnievas@gmail.com>
2025-10-09 17:40:53 -0300

    feat(General): :sparkles: Refactor routing to include Home and Error pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import Login from "./pages/auth/login/Login"
 import AuthLayout from "./layout/AuthLayout"
+import Home from "./pages/home/Home"
+import Error from "./pages/error/Error"
 
 function App() {
   return (
     <main>
       <Routes>
-        <Route path="/" element={<h1>Social pet!</h1>} />
-        <Route path="/auth" element={<AuthLayout />}>
+        <Route path="/" element={<AuthLayout />}>
+          <Route path="" element={<Home />} />
           <Route path="login" element={<Login />} />
+          <Route path="error" element={<Error />} />
         </Route>
+
+        <Route path="/*" element={<Navigate to="/error" replace />} />
       </Routes>
     </main>
   )
