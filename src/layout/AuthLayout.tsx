c7b3d9cb84eb4b8157a6d696a1fbe44fed05e8d5
Nievas2 <angelgabrielnievas@gmail.com>
2025-10-09 17:40:02 -0300

    fix(Auth): :lipstick: Fix center content

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -8,7 +8,7 @@ export default function AuthLayout() {
       <HeaderAuth />
       
       <div className="min-h-screen bg-gray-50 flex items-center justify-center p-0 md:p-8">
-        <div className="w-full min-h-screen md:min-h-0 md:max-w-md bg-white md:rounded-2xl md:shadow-xl">
+        <div className="w-full min-h-screen flex flex-col justify-center items-center md:block md:min-h-0 md:max-w-md bg-white md:rounded-2xl md:shadow-xl">
           <Outlet />
         </div>
       </div>
