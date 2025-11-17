6cb811f34a69e77ff5c50a14aef5a68017d46694
Nievas2 <angelgabrielnievas@gmail.com>
2025-11-17 15:40:05 -0300

    feat(Login): :sparkles: fix unit tests

--- src/pages/auth/login/Login.tsx
+++ src/pages/auth/login/Login.tsx
@@ -1,10 +1,11 @@
 import { loginSchema } from "@/utils/schemas/loginSchema"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { Link } from "react-router-dom"
-import { Icon } from "@iconify/react"
+import { Controller, useForm } from "react-hook-form"
 import { memo, useState } from "react"
 import useLogin from "@/hooks/auth/useLogin"
+import FormInput from "@/components/ui/FormInput"
+import { Icon } from "@iconify/react"
+import { Link } from "react-router-dom"
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false)
@@ -13,13 +14,11 @@ const Login = () => {
   const {
     handleSubmit,
     formState: { errors },
-    register,
+    control,
   } = useForm({
-    defaultValues: {
-      email: "",
-      password: "",
-    },
+    defaultValues: { email: "", password: "" },
     resolver: zodResolver(loginSchema),
+    mode: "onChange", // Add this for real-time validation
   })
 
   const loginFunction = handleSubmit(async (values) => {
@@ -31,141 +30,100 @@ const Login = () => {
   })
   return (
     <div className="flex flex-col gap-8 min-h-screen md:min-h-0">
-      <Link
-        className="flex md:hidden items-center p-4 text-gray-600 hover:text-gray-900"
-        to="/"
-      >
-        <Icon icon="mingcute:left-fill" className="size-7 font-bold" />
-        <span className="sr-only">Volver</span>
-      </Link>
-
       <div className="flex flex-col gap-6 p-6 justify-center items-center">
-        <img src="logo-name.svg" alt="Logo" className="w-40" />
+        <img src="logo-name.svg" alt="Logo" className="w-40 md:w-18" />
         {/* Form */}
-        <form onSubmit={loginFunction} className="flex flex-col gap-4">
-          {/* Email Input */}
-          <div className="relative">
-            <div className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600">
-              <Icon icon="mdi:person" className="size-5" />
-            </div>
-
-            {errors.email && (
-              <div className="group">
-                <div className="absolute right-4 top-1/2 -translate-y-1/2 text-red-error">
-                  <Icon icon="mdi:alert-circle" className="size-5" />
-                </div>
-                <div className="absolute right-7 top-2/3 -translate-y-1/2 mb-2 hidden group-hover:block bg-black text-white text-xs px-2 py-1 rounded">
-                  {errors.email.message}
-                </div>
-              </div>
+        <form onSubmit={loginFunction} className="flex flex-col gap-4 w-full">
+          {/* Email FormInput */}
+          <Controller
+            name="email"
+            control={control}
+            render={({ field }) => (
+              <FormInput
+                type="email"
+                label="Email"
+                placeholder="Email"
+                {...field} // Spreads value, onChange, etc. from Controller
+                error={errors.email}
+                disabled={loading}
+              />
             )}
+          />
 
-            <input
-              className={`w-full h-14 pl-12 pr-4 border border-gray-300 rounded-lg text-gray-900 placeholder-gray-500 focus:border-transparent focus:outline-none focus:ring-2 ${
-                errors.email
-                  ? "focus:ring-red-error border-red-error"
-                  : "focus:ring-purple-500"
-              }`}
-              type="email"
-              placeholder="Email"
-              {...register("email")}
-              disabled={loading}
-            />
-          </div>
-
-          {/* Password Input */}
-          <div className="relative ">
-            <div className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-600">
-              <Icon icon="mdi:lock" className="size-5" />
-            </div>
-            {errors.password && (
-              <div className="group">
-                <div className="absolute right-4 top-1/2 -translate-y-1/2 text-red-error">
-                  <Icon icon="mdi:alert-circle" className="size-5" />
-                </div>
-                <div className="absolute right-7 top-2/3 -translate-y-1/2 mb-2 hidden group-hover:block bg-black text-white text-xs px-2 py-1 rounded">
-                  {errors.password.message}
-                </div>
-              </div>
+          {/* Password FormInput */}
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => (
+              <FormInput
+                type={showPassword ? "text" : "password"}
+                label="Password"
+                placeholder="Contraseña"
+                {...field}
+                error={errors.password}
+                disabled={loading}
+              />
             )}
-
-            <input
-              type={showPassword ? "text" : "password"}
-              className={`w-full h-14 pl-12 pr-4 border border-gray-300 rounded-lg text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:border-transparent ${
-                errors.password
-                  ? "focus:ring-red-error border-red-error"
-                  : "focus:ring-purple-500"
-              }`}
-              placeholder="Contraseña"
-              {...register("password")}
-              disabled={loading}
-            />
-          </div>
+          />
 
           {/* Remember me checkbox */}
-          <label className="flex items-center gap-2 cursor-pointer">
+          <label className="flex items-center justify-start gap-2 cursor-pointer">
             <input
               type="checkbox"
               checked={showPassword}
               onChange={(e) => setShowPassword(e.target.checked)}
-              className="w-4 h-4 text-purple-600 border-gray-300 rounded focus:ring-purple-500"
               disabled={loading}
             />
             <span className="text-sm font-semibold">Mostrar contraseña</span>
           </label>
 
-          {/* Errors */}
-          <div className="md:hidden flex flex-col gap-2">
-            {errors.email && (
-              <p className="text-red-error text-sm font-bold">
-                {errors.email.message}
-              </p>
-            )}
-            {errors.password && (
-              <p className="text-red-error text-sm font-bold">
-                {errors.password.message}
-              </p>
-            )}
-          </div>
-
           {/* Error API response */}
           {error && <p className="text-red-error text-sm font-bold">{error}</p>}
 
-          {/* Submit Button */}
-          <button
-            type="submit"
-            disabled={loading}
-            className="w-full h-14 bg-violet-main hover:bg-violet-main/80 text-white font-semibold rounded-lg transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center gap-2"
-          >
-            {loading ? (
-              <div className="flex items-center gap-2">
-                <Icon icon="mdi:loading" className="size-5 animate-spin" />
-                Cargando...
-              </div>
-            ) : (
-              "Entrar"
-            )}
-          </button>
+          <div className="flex flex-col gap-2 w-full">
+            {/* Submit Button */}
+            <button
+              type="submit"
+              disabled={loading}
+              className="w-full h-14 bg-violet-main hover:bg-violet-main/80 text-white font-semibold rounded-full transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center gap-2 shadow-lg shadow-gray-500"
+            >
+              {loading ? (
+                <div className="flex items-center gap-2">
+                  <Icon icon="mdi:loading" className="size-5 animate-spin" />
+                  Cargando...
+                </div>
+              ) : (
+                "Login"
+              )}
+            </button>
 
-          {/* Forgot password link */}
-          <div className="text-center">
-            <a href="#" className="text-sm font-semibold">
+            {/* Forgot password link */}
+            <Link
+              to="#"
+              className="text-xs font-semibold text-violet-main text-end"
+            >
               ¿Olvidaste la contraseña?
-            </a>
+            </Link>
           </div>
+
+          <p className="text-xs">
+            Al continuar aceptas los{" "}
+            <a href="#" className="text-blue-600">
+              Términos y Condiciones y Políticas de privacidad de Huellas.
+            </a>
+          </p>
+
+          {/* Google button */}
+          <button
+            type="button"
+            disabled={loading}
+            className="w-full h-14 text-gray-700 font-semibold rounded-full transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center gap-2 shadow-lg shadow-gray-300"
+          >
+            <Icon icon="devicon:google" className="size-5" />
+            Continuar con Google
+          </button>
         </form>
       </div>
-
-      {/* Register link */}
-      <p className="text-center text-sm text-gray-600 border-t border-gray-300 py-6 mx-4">
-        Si no tienes una cuenta,{" "}
-        <Link
-          to="/register"
-          className="font-semibold underline md:no-underline underline-offset-2 hover:underline"
-        >
-          regístrate aquí
-        </Link>
-      </p>
     </div>
   )
 }
