26869f5fe273513b10555a63dc6a3976aeefab02
Nievas2 <104918140+Nievas2@users.noreply.github.com>
2025-11-18 10:13:28 -0300

    Add password recovery (#5)
    * feat: (PasswordRecovery): :sparkles: Add password recovery page. Add email with code.
    
    * feat: (PasswordRecovery): :sparkles: Implement password recovery flow with email, code verification, and password reset forms.
    
    * fix(SendEmail): :bug: Fix error message
    
    * fix(Auth): :lipstick: Fix button style
    
    * style(SendEmailForm): :lipstick: Add cursor pointer to the button
    
    * feat(Auth): :sparkles: Add verify button service
    
    * feat(Auth): :bug: Fix resetPassword and verifyCode functions
    
    * feat(SendEmailForm): :sparkles: Add tests for SendEmailForm component and improve error handling
    
    * feat(CheckCodeForm): :sparkles: Implement CheckCodeForm component with verification and resend functionality
    
    * feat(PasswordForm): :sparkles: Add tests for PasswordForm component and implement success state handling

--- src/services/AuthServices.ts
+++ src/services/AuthServices.ts
@@ -4,6 +4,11 @@ interface loginProps {
   email: string
   password: string
 }
+
+interface verifyCodeProps {
+  email: string
+  code: string
+}
 export async function login({ email, password }: loginProps) {
   const response = await axiosInstance.get(
     `users?email=${email}&password=${password}`
@@ -11,6 +16,50 @@ export async function login({ email, password }: loginProps) {
   return response
 }
 
+export async function sendPasswordResetEmail({ email }: { email: string }) {
+  function generateCode() {
+    return Math.floor(100000 + Math.random() * 900000).toString()
+  }
+
+  const response = await axiosInstance.get(`users?email=${email}`)
+
+  if (response.status === 200 && response.data.length > 0) {
+    const user = response.data[0]
+    const code = generateCode()
+
+    const updateResponse = await axiosInstance.patch(`users/${user.id}`, {
+      code: code,
+    })
+
+    return updateResponse
+  } else {
+    throw new Error("Usuario no encontrado")
+  }
+}
+
+
+export async function verifyCode({
+  email,
+  code,
+}: verifyCodeProps) {
+  const response = await axiosInstance.get(`users?email=${email}&code=${code}`)
+  return response
+}
+
+export async function resetPassword({ email, password }: loginProps) {
+  const user = await axiosInstance.get(`users?email=${email}`)
+
+  if (user.status === 200 && user.data.length > 0) {
+    const userId = user.data[0].id
+
+    const updateResponse = await axiosInstance.patch(`users/${userId}`, {
+      password: password,
+    })
+    return updateResponse
+  } else {
+    throw new Error("Usuario no encontrado")
+  }
+}
 interface registerProps {
   name: string
   email: string
